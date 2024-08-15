9034ef7af5a9faebf1b7e01f693d0f35bfd40b26
rtrend96 <rtrend96@gmail.com>
2024-08-15 00:14:58 -0500

    Add project files.

---
+++ JokeApp/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokeApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Jokequestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
